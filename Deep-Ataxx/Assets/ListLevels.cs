b59a6561cf23b45f04ccfc89e8616600e681ede7
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2022-05-16 00:36:38 -0600

    Adressables

---
+++ Deep-Ataxx/Assets/ListLevels.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using Cox.Infection.Management;
+using UnityEngine.UIElements;
+
+public class ListLevels : MonoBehaviour
+{
+    public Level[] levelList;
+    
+
+    private void Awake() {
+        
+    }
+
+    public void CreateLevelList(){
+        
+
+    }
+}
